2db536d1aad03b4e26d054f1969dd69d75abb598
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-22 14:01:20 +0200

    Fixing the declaration and comment/deocumentation

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,4 @@
--- creates an index and returns the first letter of name
+-- creates an index idx_name_first_score
+-- on the table, order the names by first letter 
 CREATE INDEX idx_name_first_score
 ON names (name, (1));
