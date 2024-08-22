1de731be0bdd5ffabf215041567d49f4e1277344
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-22 13:29:32 +0200

    Adding a parenthesis in line 2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as thier style ranked by longevity
-SELECT band_name, (IFNULL(split, '2020') - formed_ AS lifespan
+SELECT band_name, (IFNULL(split, '2020')) - formed_ AS lifespan
 	FROM metal_bands
 	WHERE FIND_IN_SET('Glam rock', INFULL(style, "")) > 0
 	ORDER BY lifespan DESC;
