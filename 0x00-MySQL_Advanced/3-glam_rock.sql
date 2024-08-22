0c45df52df9986b3cd781148904246c6114d43e0
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-22 14:24:21 +0200

    Fixing the code for 3, by removing unneccessary brackets

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- lists all bands with Glam rock as thier style ranked by longevity
-SELECT band_name, (IFNULL(split, '2022')) - formed_ AS lifespan
-	FROM metal_bands
-	WHERE FIND_IN_SET('Glam rock', INFULL(style, "")) > 0
-	ORDER BY lifespan DESC;
+SELECT band_name,
+	(IFNULL(split, '2022') - formed) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan DESC;
