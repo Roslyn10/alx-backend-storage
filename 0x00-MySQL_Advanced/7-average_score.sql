310ba22c13b4adb631b0eb0dcd66128b8ae088ee
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-22 14:11:38 +0200

    Adding the code for 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,4 +1,21 @@
 -- creates a stored procedure that computes and..
 -- .. stores the average score for a student
-CREATE PROCEDURE user_id
-RETURN AVG(score);
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score DECIMAL(5,2);
+    
+    SELECT AVG(score)
+    INTO avg_score
+    FROM scores
+    WHERE user_id = user_id;
+    
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END //
+
+DELIMITER ;
