35d4859dd84398b11bef42b52dc5bc9d46dfa500
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-28 20:43:34 +0200

    Adding the code for 1. which creates a method that takes a key and converts data to desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A module that contains a redis class"""
 
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 import redis
 
@@ -25,3 +25,32 @@ class Cache:
         randomkey = str(uuid4())
         self._redis.set(randomkey,data)
         return randomkey
+
+    def get(self, key :str, 
+            fn: Optional[Callable] = None) -> Union[str, bytes, int,float]:
+        """
+        Converts data to desired format
+        """
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """
+        Parametrize Cache.get with the correct conversion function
+        """
+        val = self._redis.get(key)
+        return val.decode("utf-8")
+
+
+    def get_in(self, key: str) -> str:
+        """
+        Parametrize Cache.get with the correct conversion function
+        """
+        val = self.redis.get(key)
+        try:
+            val = int(value.decode("utf-8"))
+        except Exception:
+            val = 0
+        return val
